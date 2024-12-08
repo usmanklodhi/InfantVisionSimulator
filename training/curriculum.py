74e1452bec021c1fcc4648185c87f4181e2764fc
Usman KL <usmanklodhii@gmail.com>
2024-12-08 16:51:27 +0100

    patch: testing import errors workaround

--- training/curriculum.py
+++ training/curriculum.py
@@ -3,12 +3,13 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim.lr_scheduler import StepLR
-from models import resnet18
+# from models import resnet18
 from src import dataloader as dl, preprocessed_dataset as ppd
 from my_datasets import tiny_imagenet as ti
 import os
 from training import utils as ut
 from training import train as tr
+from torchvision.models import resnet18
 
 # Curriculum learning and parameters
 stages = ['young', 'mid', 'old']
@@ -20,6 +21,11 @@ lr = 0.01
 # Device setup
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+def get_resnet18_LOCAL(num_classes=200):
+    model = resnet18(pretrained=False)
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
+    return model
+
 def main():
     os.makedirs('outputs/models', exist_ok=True)
 
@@ -29,7 +35,7 @@ def main():
     val_dataset = ppd.PreprocessedDataset(val_data, transform=ti.old_transform)
     val_dataloader = dl.create_dataloader_v3(val_dataset, batch_size=batch_size)
 
-    model = resnet18.get_resnet18().to(device)
+    model = get_resnet18_LOCAL().to(device)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4)
