0a7f8ede47d2a564567741cc698ec608c2c26487
Sohan Saldanha <sohanjs111@gmail.com>
2024-12-09 16:29:57 +0100

    Patch for Visual acuity

--- transforms/visual_acuity.py
+++ transforms/visual_acuity.py
@@ -10,8 +10,8 @@ class VisualAcuityTransform:
         max_blur = 15  # Corresponds to 20/600 vision
         min_blur = 0  # Corresponds to 20/20 vision
 
-        # Update the decay formula to reflect 36 months timeline
-        self.blur_radius = max(min_blur, max_blur * math.exp(-age_in_months / 12))  # 12-month time constant
+        # Update the decay formula to reflect 12 months timeline
+        self.blur_radius = max(min_blur, max_blur * math.exp(-age_in_months / 4))  # 12-month time constant
 
     def __call__(self, image):
         return image.filter(ImageFilter.GaussianBlur(self.blur_radius))
