ba93df4a4996500c18fd3330ea7e7dc3267babba
Usman KL <usmanklodhii@gmail.com>
2025-01-01 08:58:57 +0100

    more epochs

--- configuration/setting.py
+++ configuration/setting.py
@@ -8,7 +8,7 @@ AGES = [3, 6, 9, 12]
 
 ## Project PART 2
 DATASET_PATH="my_datasets/tiny-imagenet-200"
-EPOCHS = 8
+EPOCHS = 30
 BATCH_SIZE = 400
 LEARNING_RATE = 0.01
 NUM_CLASSES = 200  # Tiny ImageNet has 200 classes
